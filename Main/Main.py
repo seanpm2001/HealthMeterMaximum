8d31ee701758fd1c006c997f7645f8cd321bb2b4
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-31 20:46:00 -0800

    Early prototype
    An early prototype of the health meter.

---
+++ Main/Main.py
@@ -0,0 +1,88 @@
+# Main thread for health meter
+''' Import when defined, uncomment when defined
+import heartRateMonitor
+import staminaMeter
+import foodMeter
+import hydroMeter
+import energyMeter
+import charImageBool
+import moodMeter
+import ironMeter
+import stressMeter
+import hygieneMeter
+import sedationMeter
+import euphoricMeter # Euphoria
+import proteinMeter
+import bloodLevelMeter
+import oxygenMeter
+import hitpointsMeter
+import damagePerSecondMeter
+import weightMeter # LB, KG
+import heightMeter # CM, IN, FT, M
+import eyesightMeter
+import smellMeter
+import libidoMeter # Sex drive, horniness
+import processesPerSecondMeter # Number of active processes in the mind, 10-60000
+import sleepModeMeter
+import painMeter
+import nauseaMeter
+import insulinMeter
+import bowelMeter
+import toothMeter
+import noseStatusMeter
+import earStatusMeter
+import mouthStatusMeter
+import headhairStatusMeter
+import bodyOdorMeter
+import cardioMeter
+import muscleMeter
+import nostalgiaMeter
+import depressionMeter
+import motivationMeter
+import flatulanceMeter
+import focusMeter
+import eyeTracker
+import motionSicknessMeter
+import terrorMeter
+import fearMeter
+import rageMeter
+import angerMeter
+import bladderMeter
+import blueLightMeter
+import calmMeter
+import sugarMeter
+import bloodSugarMeter
+import saltMeter
+import bloodSaltMeter
+import bloodAlcoholMeter
+import bloodTypeMeter
+import loveMeter
+import phobiaMeter
+import ptsdMeter # (Post Traumatic Stess Disorder)
+import fluMeter
+import commonColdMeter
+import allergyMeter
+import tmjMeter # (Temporomandibular Joint Dysfunction)
+import caffeineMeter
+import constipationStatus
+import burnMeter
+import fallDamageStatus
+import coldStatus
+import hotStatus
+import boneMeter
+import embarrassmentMeter
+import empathyMeter
+import helpfulnessMeter
+import hypothermiaMeter
+import lonelinessMeter
+import excitementMeter
+'''
+damagePerSecond = float(0.0)
+def staminaMeter():
+
+def heartRateMonitorUI():
+
+def damagePerSecondMeter():
+	print ("Damage per second: " + str(damagePerSecond))
+break
+# End of script
